ad274901f4ca61338ecfeb3098e4edf06553af32
Anil Kashyap <anilkashyap@hotmail.com>
2021-06-13 19:30:33 +0530

    second commit

--- a.java
+++ a.java
@@ -1,1 +1,12 @@
-gndgnkdng
+import java.lang.Scanner;
+
+class Demo{
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter the string :  ");
+		String s = sc.nextLine();
+		
+	}
+
+}
