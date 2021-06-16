08970b1a0bee12c16ff191cf5ab5f4203e1a654d
Techguru-anil <anilkashyap@hotmail.com>
2021-06-17 00:51:48 +0530

    changes logic

--- a.java
+++ a.java
@@ -1,12 +1,16 @@
 import java.lang.Scanner;
 
 class Demo{
-	
+	gdjjdjghkj 
+		gkjggjk
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		System.out.print("enter the string :  ");
+		System.out.print("enter the strinkhggkjgkgkgjggkjg :  ");
 		String s = sc.nextLine();
 		
 	}
+	hlgg
+		hhklh
+		klhlhlhh
 
 }
